d1d6a31b877a464e8d808df30d7733c3a45ff93b
Ivan Solodilov <optim000000@gmail.com>
2022-10-05 17:58:27 +0400

    Add meander

---
+++ f_specs/core/source/meander/core_source_meander_exec.c
@@ -0,0 +1,22 @@
+#include "core_source_meander.h"
+
+void core_source_meander_exec(
+    core_source_meander_outputs_t *o,
+    const core_source_meander_params_t *p,
+    core_source_meander_state_t *state,
+    const core_source_meander_injection_t *injection
+)
+{
+    // TODO: calculate absolute time
+    if(state->time < (p->period * p->duty)) {
+        o->out = 0;
+    } else {
+        o->out = 1;
+    }
+    state->time += injection->dt;
+    if(state->time > p->period) {
+        o->out = 0;
+        state->time = 0;
+    }
+}
+
---
+++ f_specs/core/source/meander/declaration.py
@@ -0,0 +1,44 @@
+from ctlst import *
+
+Function(
+    name='core.source.meander',
+    title=LocalizedString(
+        en='Meander with constant period'
+    ),
+    description=None,
+    parameters=[
+        Parameter(
+            name='period',
+            title='Meadner period',
+            value_type='core.type.f64',
+            constraints=[
+                ThisValue() > 0
+            ]
+        ),
+        Parameter(
+            name='duty',
+            title='Meadner duty cycle',
+            value_type='core.type.f64',
+            constraints=[
+                ThisValue() > 0
+            ]
+        ),
+    ],
+    outputs=[
+        Output(
+            name='out',
+            title='Meander value',
+            value_type='core.type.f64'
+        )
+    ],
+    state=[
+        Variable(
+        name='time',
+        title='Free run time accumulator',
+        value_type='core.type.f64'
+        )
+    ],
+    injection=Injection(
+        timedelta=True
+    )
+)
