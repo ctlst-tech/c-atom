269d37fb5ba6fd55f04ae34c0fa4dc620be02fd5
Ivan Makarov <ivan_makarov@icloud.com>
2023-01-19 21:03:07 +0400

    Add vector_u8 atomic type

---
+++ atomics/core/type/vector_u8/declaration.py
@@ -0,0 +1,9 @@
+from fspeclib import *
+
+Vector(
+    name='core.type.vector_u8',
+    elem_type='core.type.u8',
+    title=LocalizedString(
+        en='Vector of 8-bit unsigned integral number'
+    ),
+)
