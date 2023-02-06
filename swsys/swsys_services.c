8d9baccbe05891268fc7e0e8f07f82d1e5358d12
Ivan Makarov <ivan_makarov@icloud.com>
2023-02-06 16:36:04 -0500

    Increase number of topics for bus for SDTL services

--- swsys/swsys_services.c
+++ swsys/swsys_services.c
@@ -108,7 +108,8 @@ static swsys_rv_t sdtl_init_and_start(const swsys_service_t *s) {
 
     if (!sdtl_bus_inited) {
         // TODO get proper metrics for needed topics num
-        eswb_rv_t erv = eswb_create(SDTL_BUS_NAME, eswb_inter_thread, 10 + 16 * ch_num);
+#       define MAX_SDTLS 3
+        eswb_rv_t erv = eswb_create(SDTL_BUS_NAME, eswb_inter_thread, (10 + 16 * ch_num) * MAX_SDTLS);
         if (erv != eswb_e_ok) {
             return swsys_e_service_fail;
         }
